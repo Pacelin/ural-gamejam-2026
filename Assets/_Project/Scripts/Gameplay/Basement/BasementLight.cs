b093a27a43598fa7387f27a31771b4d814f4b3a6
Pacelin <Warfacer2020@gmail.com>
2026-03-25 14:27:40 +0300

    update sounds build

--- Assets/_Project/Scripts/Gameplay/Basement/BasementLight.cs
+++ Assets/_Project/Scripts/Gameplay/Basement/BasementLight.cs
@@ -6,9 +6,13 @@ namespace Project.Gameplay.Basement
 {
     public class BasementLight : MonoBehaviour
     {
-        [SerializeField] private float[] _delays;
-        [SerializeField] private float[] _durations;
-        [SerializeField] private float[] _intensities;
+        [SerializeField] private Vector2 _randomDelay;
+        [SerializeField] private Vector2 _randomIntensities;
+        [SerializeField] private Vector2 _randomLowIntensities;
+        [SerializeField] private Vector2 _lowDelayOnStart;
+        [SerializeField] private Vector2Int _randomTickCount;
+        [SerializeField] private Vector2 _randomDurations;
+        [SerializeField] private Vector2 _randomLastIntensity;
         [SerializeField] private Light _light;
         [Space] 
         [SerializeField] private Transform _soundPoint;
@@ -16,6 +20,7 @@ namespace Project.Gameplay.Basement
         [SerializeField] private SoundEvent _onSound;
         [Space]
         [SerializeField] private Vector2 _randomTickDelays;
+        [SerializeField] private Vector2 _lowDelayTick;
         [SerializeField] private float _lowTickIntensity;
         [SerializeField] private float _tickDuration;
         [Space]
@@ -42,10 +47,7 @@ namespace Project.Gameplay.Basement
         {
             _tickCooldown -= Time.deltaTime;
             if (_tickCooldown <= 0)
-            {
-                UpdateCooldown();
                 Tick();
-            }
         }
 
         public void Activate()
@@ -57,31 +59,46 @@ namespace Project.Gameplay.Basement
             _soundEventInstance.Start();
             
             var seq = DOTween.Sequence(_light);
-            for (int i = 0; i < _intensities.Length; i++)
+            var count = Random.Range(_randomTickCount.x, _randomTickCount.y);
+            for (int i = 0; i < count; i++)
             {
-                seq.AppendInterval(_delays[i]);
+                var delay = Random.Range(_randomDelay.x, _randomDelay.y);
+                var intensity = i == count - 1
+                    ? Random.Range(_randomLastIntensity.x, _randomLastIntensity.y)
+                    : Random.Range(_randomIntensities.x, _randomIntensities.y);
+                var lowIntensity = Random.Range(_randomLowIntensities.x, _randomLowIntensities.y);
+                var duration = Random.Range(_randomDurations.x, _randomDurations.y) / 2;
+                var lowDelay = Random.Range(_lowDelayOnStart.x, _lowDelayOnStart.y);
+                seq.AppendInterval(delay);
                 seq.AppendCallback(() => _tickSound.PlayOneShotInPoint(_soundPoint.position));
-                seq.Append(_light.DOIntensity(_intensities[i], _durations[i]));
+                seq.Append(_light.DOIntensity(lowIntensity, duration));
+                seq.AppendInterval(lowDelay);
+                seq.AppendCallback(() => _tickSound.PlayOneShotInPoint(_soundPoint.position));
+                seq.Append(_light.DOIntensity(intensity, duration));
             }
         }
 
         private void Tick()
         {
-            _tickSound.PlayOneShotInPoint(_soundPoint.position);
             var halfDuration = _tickDuration / 2f;
             var startIntensity = _light.intensity;
+            var lowDelay = Random.Range(_lowDelayTick.x, _lowDelayTick.y);
             DOTween.Sequence(_light)
+                .AppendCallback(() => _tickSound.PlayOneShotInPoint(_soundPoint.position))
                 .Append(_light.DOIntensity(_lowTickIntensity, halfDuration))
+                .AppendInterval(lowDelay)
+                .AppendCallback(() => _tickSound.PlayOneShotInPoint(_soundPoint.position))
                 .Append(_light.DOIntensity(startIntensity, halfDuration));
+            UpdateCooldown(lowDelay + halfDuration * 2);
         }
 
-        private void UpdateCooldown()
+        private void UpdateCooldown(float withDelay)
         {
             var randomValue = Random.value;
             if (randomValue <= _doubleTickChance)
-                _tickCooldown = Random.Range(_randomTickDelays.x, _randomTickDelays.y);
+                _tickCooldown = withDelay + Random.Range(_randomTickDelays.x, _randomTickDelays.y);
             else
-                _tickCooldown = Random.Range(_doubleTickDelays.x, _doubleTickDelays.y);
+                _tickCooldown = withDelay + Random.Range(_doubleTickDelays.x, _doubleTickDelays.y);
         }
     }
 }
