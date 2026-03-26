8e006396a590d7eda4261a41b2386eaf81d1f753
Pacelin <Warfacer2020@gmail.com>
2026-03-26 15:15:53 +0300

    bunker update

---
+++ Assets/_Project/Scripts/Editor/CameraEditorUtils.cs
@@ -0,0 +1,49 @@
+﻿using Project.Gameplay.Movement;
+using UnityEditor;
+using UnityEngine;
+
+namespace Project.Editor
+{
+    public static class CameraEditorUtils
+    {
+        [MenuItem("GameObject/Align Camera To Object")]
+        private static void SetCameraTo()
+        {
+            var selectedGo = Selection.activeGameObject;
+            if (!selectedGo)
+                return;
+
+            var goTransform = selectedGo.transform;
+            var cameraTransform = Object.FindAnyObjectByType<MovementCameraController>().transform;
+            cameraTransform.position = goTransform.position;
+            cameraTransform.rotation = goTransform.rotation;
+        }
+        
+        [MenuItem("GameObject/Align Camera To Object", true)]
+        private static bool SetCameraToValidate()
+        {
+            var selectedGo = Selection.activeGameObject;
+            return selectedGo;
+        }
+        
+        [MenuItem("GameObject/Set Object To Camera")]
+        private static void SetObjectToCamera()
+        {
+            var selectedGo = Selection.activeGameObject;
+            if (!selectedGo)
+                return;
+
+            var goTransform = selectedGo.transform;
+            var cameraTransform = Object.FindAnyObjectByType<MovementCameraController>().transform;
+            goTransform.position = cameraTransform.position;
+            goTransform.rotation = cameraTransform.rotation;
+        }
+        
+        [MenuItem("GameObject/Set Object To Camera", true)]
+        private static bool SetObjectToCameraValidate()
+        {
+            var selectedGo = Selection.activeGameObject;
+            return selectedGo;
+        }
+    }
+}
