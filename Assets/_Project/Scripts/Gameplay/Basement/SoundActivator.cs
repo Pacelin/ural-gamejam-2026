2df98593dc70e7f102d440269e97cecc01cf674a
Pacelin <Warfacer2020@gmail.com>
2026-03-27 18:38:11 +0300

    update a lot

---
+++ Assets/_Project/Scripts/Gameplay/Basement/SoundActivator.cs
@@ -0,0 +1,17 @@
+﻿using Plugins.Audio;
+using Project.Gameplay.Interactables;
+using UnityEngine;
+
+namespace Project.Gameplay.Basement
+{
+    public class SoundActivator : PuzzleActivator
+    {
+        [SerializeField] private Transform _soundPoint;
+        [SerializeField] private SoundEvent _sound;
+
+        public override void Activate()
+        {
+            _sound.PlayOneShotInPoint(_soundPoint.position);
+        }
+    }
+}
