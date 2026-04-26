ffdbada08eccff500a7a765dff5d1d69be1592bc
Pacelin <Warfacer2020@gmail.com>
2026-04-26 17:29:27 +0300

    some updates

--- Assets/_Project/Scripts/Gameplay/CollectablesModel.cs
+++ Assets/_Project/Scripts/Gameplay/CollectablesModel.cs
@@ -1,4 +1,7 @@
-﻿namespace Project.Editor.Gameplay
+﻿using System.Collections.Generic;
+using Project.Gameplay.Inventory;
+
+namespace Project.Editor.Gameplay
 {
     public class CollectablesModel
     {
@@ -6,19 +9,27 @@
 
         public int Current => _current;
         public int Required => _required;
+        public int LastCollected => _lastCollected;
         
         private int _current;
-        
+        private int _lastCollected;
+
+        private readonly HashSet<int> _collected;
         private readonly int _required;
         
         public CollectablesModel(int current, int required)
         {
             _current = current;
             _required = required;
+            _collected = new HashSet<int>();
         }
 
-        public void Add()
+        public bool IsCollected(int index) => _collected.Contains(index);
+        
+        public void Add(int collectableIndex)
         {
+            _collected.Add(collectableIndex);
+            _lastCollected = collectableIndex;
             _current++;
             OnChanged?.Invoke();
         }
--- Assets/_Project/Scripts/Gameplay/CollectablesView.cs
+++ Assets/_Project/Scripts/Gameplay/CollectablesView.cs
@@ -1,14 +1,73 @@
-﻿using DG.Tweening;
+﻿using System;
+using DG.Tweening;
+using Project.Gameplay.Interactables;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using VContainer;
 
 namespace Project.Editor.Gameplay
 {
+    public class CollectablesWindow : NotInteractableObject
+    {
+        [SerializeField] private Button[] _closeButtons;
+        [SerializeField] private CollectableSelector[] _selectors;
+        [SerializeField] private GameObject[] _contents;
+
+        private CollectablesModel _collectablesModel;
+        
+        protected override void Initialize(IObjectResolver resolver)
+        {
+            _collectablesModel = resolver.Resolve<CollectablesModel>();
+        }
+        
+        private void OnEnable()
+        {
+            foreach (var closeButton in _closeButtons)
+                closeButton.onClick.AddListener(Hide);
+            foreach (var )
+        }
+
+        private void OnDisable()
+        {
+            foreach (var closeButton in _closeButtons)
+                closeButton.onClick.RemoveListener(Hide);
+        }
+        
+        private void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    public class CollectableSelector : MonoBehaviour
+    {
+        public Button Button => _button;
+        
+        [SerializeField] private Button _button;
+        [SerializeField] private GameObject _activeWhenCollected;
+        [SerializeField] private GameObject _activeWhenNotCollected;
+        [SerializeField] private GameObject _activeWhenSelected;
+
+        public void SetSelected(bool selected)
+        {
+            _activeWhenSelected.SetActive(selected);
+        }
+
+        public void SetCollected(bool collected)
+        {
+            _activeWhenCollected.SetActive(collected);
+            _activeWhenNotCollected.SetActive(!collected);
+        }
+    }
+    
     public class CollectablesView : MonoBehaviour
     {
+        [SerializeField] private Button _button;
+        [SerializeField] private CollectablesWindow _window;
         [SerializeField] private TMP_Text _text;
         [SerializeField] private Transform _transform;
-
+        
         private void OnDestroy() => DOTween.Kill(_transform);
 
         public void SetText(int current, int required) => _text.text = current + "/" + required;
